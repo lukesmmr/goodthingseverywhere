4adec4f5a2576491ea3d10274a41c4177822dc6b
Lukas <lukas@goodthingseverywhere.com>
2013-11-20 11:59:12 +1100

    add comment count

--- wp-content/themes/goodthings/templates/entry-meta.php
+++ wp-content/themes/goodthings/templates/entry-meta.php
@@ -2,6 +2,9 @@
 	<time class="published journal-date" datetime="<?php echo get_the_time('c'); ?>">
 		<?php echo get_the_date('j. M y'); ?>
 	</time>
+	<span class="comment-count">
+  		<?php comments_number( '', '1 Comment', '(% comments' ); ?>
+	</span>
 	<div class="journal-category">
 		<?php the_category(', '); ?>
 		<span class="glyphicon glyphicon-globe"></span>
