7d3c01075e7620e915f1c1e83f9a9147a92b3c8e
Goodthings Server <lukas@goodthingseverywhere.com>
2013-11-25 22:23:31 +0100

    add google analytics (temporary, universal analytics conversion coming up)

--- wp-content/themes/goodthings/templates/footer.php
+++ wp-content/themes/goodthings/templates/footer.php
@@ -14,6 +14,20 @@
       <span class="footer-comment">&copy; <?php echo date('Y'); ?></span>
   	</div>
 </footer>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-69460-22']);
+  _gaq.push(['_setDomainName', 'goodthingseverywhere.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 <script src="//use.typekit.net/kzp6vsx.js"></script>
 <script>try{Typekit.load();}catch(e){}</script>
 <script src="http://maps.google.com/maps/api/js?sensor=false&v=3&libraries=geometry"></script>
