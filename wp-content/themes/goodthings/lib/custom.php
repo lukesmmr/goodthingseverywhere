4c5558ceed50719d63da14bcf7a1db4aff473915
Lukas <lukas@goodthingseverywhere.com>
2013-12-09 12:04:41 +1100

    set default media upload settings

--- wp-content/themes/goodthings/lib/custom.php
+++ wp-content/themes/goodthings/lib/custom.php
@@ -2,3 +2,8 @@
 /**
  * Custom functions
  */
+function set_media() {
+	update_option('image_default_link_type', 'none' );
+	update_option('image_default_size', 'large' );
+}
+add_action('after_setup_theme', 'set_media');
