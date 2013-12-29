48c6b00a36b44a9aff9eeb589f677974893af8d8
Lukas <lukas@goodthingseverywhere.com>
2013-12-29 16:31:51 +1100

    add markup and styles for interactive journal map (adding temporary SVG marker)

--- wp-content/themes/goodthings/templates/content-home.php
+++ wp-content/themes/goodthings/templates/content-home.php
@@ -4,19 +4,34 @@
 	$current_loc_lat = get_post_meta($post->ID, 'current_loc_lat', true);
 	$current_loc_lng = get_post_meta($post->ID, 'current_loc_lng', true);
 	$next_loc = get_post_meta($post->ID, 'next_loc', true);
-	$home_intro = get_post_meta($post->ID, 'home_intro', true); 
+	$home_intro = get_post_meta($post->ID, 'home_intro', true);
 ?>
+<div id="journal-map">
+	 <div id="map-overlay"></div>			
+	<div id="map-canvas"></div>
+	<aside class="marker-coords">
+		<?php $wp_query= null;
+		$wp_query = new WP_Query(); $wp_query->query('showposts=-1');
+		while ($wp_query->have_posts()) : $wp_query->the_post(); 
+		$post_loc = get_post_meta($post->ID, 'post_loc', true);
+		$post_loc_lat = get_post_meta($post->ID, 'post_loc_lat', true);
+		$post_loc_lng = get_post_meta($post->ID, 'post_loc_lng', true);
+		?>
+	<span id="post-<?php the_ID(); ?>" data-post-loc="<?php echo $post_loc; ?>" data-post-loc-lat="<?php echo $post_loc_lat; ?>" data-post-loc-lng="<?php echo $post_loc_lng; ?>" data-post-url="<?php echo get_permalink(); ?>"></span>
+		<?php endwhile; ?>
+	</aside>
+	<?php wp_reset_query(); ?>
+</div>
 <div class="home-additionals">
 	<h1 class="giant-title"><?php echo $giant_title; ?></h1>
 	<div class="current-loc">
 		<span class="map-icon glyphicon glyphicon-map-marker"></span>
 		<div id="getloc" class="display-loc" data-lat="<?php echo $current_loc_lat ?>" data-lng="<?php echo $current_loc_lng; ?>">Current location: <?php echo $current_loc; ?></div>
 		<div class="next-loc">Next stop: <?php echo $next_loc ?></div>
-		<div id="journal-map">
-			<div id="map-overlay"></div>
-			<div id="map-canvas"></div>
-		</div>
 	</div>
+</div>
+<div class="journal-map-btn-container">
+	<button id="journal-map-toggle">Toggle journal map</button>
 </div>
 
 
