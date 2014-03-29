104ec9f8838313dbb8a69eeca48ee90b65ec64a2
Lukas <lukas@goodthingseverywhere.com>
2014-03-29 15:59:39 +0800

    fix home journal thumbnails on mobile (100% width)

--- wp-content/themes/goodthings/lib/scripts.php
+++ wp-content/themes/goodthings/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7b321d64d2f7983b68502ad4a9d6b864');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd249033d2d6f9258fd2f295e8b18b45f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
