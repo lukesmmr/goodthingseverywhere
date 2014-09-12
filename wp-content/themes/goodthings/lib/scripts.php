2193d28bc5fe0ffeb5a3176a37ca47df27419948
lukesmmr <lukas@goodthingseverywhere.com>
2014-09-12 16:00:34 +0100

    limit container max-width to 970px

--- wp-content/themes/goodthings/lib/scripts.php
+++ wp-content/themes/goodthings/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5c037ab197943ef76b6071d77b01e211');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'bda921b7a82c5738d22583f39adaefdf');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
