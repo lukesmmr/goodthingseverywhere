52697d8abd9c77c56544697eac044cb84d3c530f
lukesmmr <lukas@goodthingseverywhere.com>
2014-12-17 13:46:21 -0300

    add text-shadow to instagram meta location

--- wp-content/themes/goodthings/lib/scripts.php
+++ wp-content/themes/goodthings/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '81bb9c29d12177af6eefbf65dd4fa5be');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3b4640cbf78775605c58c98b85323931');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
