ea612390b90be21fb8e0f126c4e2ef406c547ec1
lukesmmr <lukas@goodthingseverywhere.com>
2014-12-17 13:50:04 -0300

    revert current project bottom margin

--- wp-content/themes/goodthings/lib/scripts.php
+++ wp-content/themes/goodthings/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3b4640cbf78775605c58c98b85323931');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5fe20b8cc3d32927de772f6a4a9f1913');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
