7e75d744ebbc0ab5b190f0385338a140d05dff8d
lukesmmr <lukas@goodthingseverywhere.com>
2015-04-10 13:37:38 -0300

    adjust caption <figure> margin

--- wp-content/themes/goodthings/lib/scripts.php
+++ wp-content/themes/goodthings/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd895504ee3e68d95449c77c8c873e921');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '87e744a0ab1fc59f2243941fe2595d6f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
