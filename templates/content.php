4ecaf7c5ef4e99241f638f23ab07259628f20978
Lukas <lukas@goodthingseverywhere.com>
2015-10-11 18:21:32 -0300

    cleaning up journal

--- templates/content.php
+++ templates/content.php
@@ -1,16 +1,14 @@
-<article <?php post_class(); ?>>
-	<div class="journal-post">
-		<div class="journal-thumbnail">
-		<?php if ( has_post_thumbnail() ) {
-				the_post_thumbnail('journal-main-thumb');
-		} ?>
-		</div>
-		<div class="journal-content">
-			<h3 class="journal-title"><a href="<?php the_permalink(); ?>" title="Read more"><?php the_title(); ?></a></h3>
-			<header>
-			    <?php get_template_part('templates/entry-meta'); ?>
-			 </header>
-			<?php the_excerpt(); ?>
-		</div>
+<article <?php post_class('journal-post'); ?>>
+	<div class="journal-thumbnail">
+	<?php if ( has_post_thumbnail() ) {
+			the_post_thumbnail('journal-main-thumb');
+	} ?>
+	</div>
+	<div class="journal-content">
+		<h3 class="journal-title"><a href="<?php the_permalink(); ?>" title="Read more"><?php the_title(); ?></a></h3>
+		<header>
+		    <?php get_template_part('templates/entry-meta'); ?>
+		 </header>
+		<?php the_excerpt(); ?>
 	</div>
 </article>
