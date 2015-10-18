fb89a64949686226831c6dab08dc174c4034c090
Lukas <lukas@goodthingseverywhere.com>
2015-10-17 21:05:56 -0300

    project type/category float fix

--- templates/project-meta.php
+++ templates/project-meta.php
@@ -2,12 +2,6 @@
 $project_year = get_post_meta($post->ID, 'project_year', true);
 ?>
 <div class="project-meta">
-  <div class="project-category">
-    <?php $category_list = wp_get_post_terms($post->ID, 'project-category' );
-      foreach($category_list as $category_single) {
-        echo '<span>' . $category_single->name . '</span>';
-    } ?>
-  </div>
   <?php $type_list = wp_get_post_terms($post->ID, 'project-type' );
   if ($type_list) :
   ?>
@@ -18,6 +12,12 @@ $project_year = get_post_meta($post->ID, 'project_year', true);
     <?php if (is_single()): ?>Project<?php endif; ?>
   </div>
   <?php endif; ?>
+  <div class="project-category">
+    <?php $category_list = wp_get_post_terms($post->ID, 'project-category' );
+      foreach($category_list as $category_single) {
+        echo '<span>' . $category_single->name . '</span>';
+    } ?>
+  </div>
   <?php if ( !is_single() ): ?>
     <div class="published project-date">
         <?php echo $project_year ?>
